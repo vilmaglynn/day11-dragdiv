11285f349dff6a844d1ff06ee5e2fa1443874f6f
Vilma Hernandez <vilmaglynn@hotmail.com>
2022-09-09 21:53:26 +0100

    drag

---
+++ apps.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container")
+let header = container.querySelector("header")
+
+function onDrag ({movementX, movementY}){
+	let getStyle = window.getComputedStyle(container);
+	let leftVal = parseInt(getStyle.left);
+	let topVal = parseInt(getStyle.top);
+container.style.left = `${leftVal + movementX}px`;
+container.style.top = `${topVal + movementY}px`;
+}
+
+header.addEventListener("mousedown", () =>{
+	header.classList.add("active");
+	header.addEventListener("mousemove", onDrag);
+})
+header.addEventListener("mouseup", () =>{
+	header.classList.remove("active");
+	header.removeEventListener("mousemove", onDrag);
+})
+
+
+
+
